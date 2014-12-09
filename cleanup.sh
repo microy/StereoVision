b3b5f4717f1f5317e391f4e8a6c753e0f07409f8
Michaël Roy <microygh@gmail.com>
2014-12-09 18:31:54 +0100

    Use the -delete option of the find command.

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,6 @@
 #
 # cleanup.sh
 #
-find . -type f -name '*.py[co]' -exec rm -v {} \;
-find . -type f -name '*~' -exec rm -v {} \;
-find . -type d -name '__pycache__' -exec rmdir -v {} \;
+find . -type f -name '*.py[co]' -delete
+find . -type f -name '*~' -delete
+find . -type d -name '__pycache__' -delete
