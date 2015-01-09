fec6f8ffebcee89113207f6437977a0a9ba5327e
Michaël Roy <microygh@gmail.com>
2015-01-09 17:43:19 +0100

    Merge synchronous capture with the Viewer module.

--- Viewer.py
+++ Viewer.py
@@ -13,8 +13,9 @@ import collections
 import time
 import threading
 import cv2
+import ctypes as ct
 import numpy as np
-import Calibration
+import Vimba
 
 
 
@@ -66,7 +67,7 @@ class Viewer( object ) :
 
 
 #
-# Vimba camera viewer
+# Vimba stereo camera viewer (asynchronous)
 #
 class StereoViewer( object ) :
 	
@@ -88,6 +89,9 @@ class StereoViewer( object ) :
 
 		# Live image of both cameras
 		self.stereo_image = np.zeros( (self.height, 2*self.width), dtype=np.uint8 )
+		
+		# Number of images saved
+		self.image_count = 0
 		
 	#
 	# Start capture and display image stream
@@ -156,3 +160,114 @@ class StereoViewer( object ) :
 		# Keyboard interruption
 		if ( cv2.waitKey(1) & 0xFF ) == 27 :
 			self.capturing = False
+
+
+#
+# Vimba stereo camera viewer (synchronous)
+#
+class StereoViewerSync( object ) :
+	
+	#
+	# Initialization
+	#
+	def __init__( self, camera_1, camera_2 ) :
+		
+		# Register the camera to display
+		self.camera_1 = camera_1
+		self.camera_2 = camera_2
+	
+		# Image parameters
+		self.width = camera_1.width
+		self.height = camera_1.height
+		
+		# Image frames
+		self.frame_1 = Vimba.VmbFrame( camera_1.payloadsize )
+		self.frame_2 = Vimba.VmbFrame( camera_1.payloadsize )
+
+		# Live image of both cameras
+		self.stereo_image = np.zeros( (self.height, 2*self.width), dtype=np.uint8 )
+		
+		# Configure frame software trigger
+		Vimba.vimba.VmbFeatureEnumSet( self.camera_1.handle, "TriggerSource", "Software" )
+		Vimba.vimba.VmbFeatureEnumSet( self.camera_2.handle, "TriggerSource", "Software" )
+
+	#
+	# Start capture and display image stream
+	#
+	def LiveDisplay( self ) :
+
+		# Number of images saved
+		image_count = 0
+		
+		# Announce the frames
+		Vimba.vimba.VmbFrameAnnounce( self.camera_1.handle, ct.byref(self.frame_1), ct.sizeof(self.frame_1) )
+		Vimba.vimba.VmbFrameAnnounce( self.camera_2.handle, ct.byref(self.frame_2), ct.sizeof(self.frame_2) )
+
+		# Start capture engine
+		Vimba.vimba.VmbCaptureStart( self.camera_1.handle )
+		Vimba.vimba.VmbCaptureStart( self.camera_2.handle )
+
+		# Start acquisition
+		Vimba.vimba.VmbFeatureCommandRun( self.camera_1.handle, "AcquisitionStart" )
+		Vimba.vimba.VmbFeatureCommandRun( self.camera_2.handle, "AcquisitionStart" )
+
+		# Live display
+		while True :
+			
+			# Queue frames
+			Vimba.vimba.VmbCaptureFrameQueue( self.camera_1.handle, ct.byref(self.frame_1), None )
+			Vimba.vimba.VmbCaptureFrameQueue( self.camera_2.handle, ct.byref(self.frame_2), None )
+			
+			# Send software trigger
+			Vimba.vimba.VmbFeatureCommandRun( self.camera_1.handle, "TriggerSoftware" )
+			Vimba.vimba.VmbFeatureCommandRun( self.camera_2.handle, "TriggerSoftware" )
+
+			# Get frames back
+			Vimba.vimba.VmbCaptureFrameWait( self.camera_1.handle, ct.byref(self.frame_1), 1000 )
+			Vimba.vimba.VmbCaptureFrameWait( self.camera_2.handle, ct.byref(self.frame_2), 1000 )
+			
+			# Check frame validity
+			if self.frame_1.receiveStatus or self.frame_2.receiveStatus :
+				continue
+			
+			# Convert frames to numpy arrays
+			image_1 = np.fromstring( self.frame_1.buffer[ 0 : self.camera_1.payloadsize ], dtype=np.uint8 ).reshape( self.height, self.width )
+			image_2 = np.fromstring( self.frame_2.buffer[ 0 : self.camera_1.payloadsize ], dtype=np.uint8 ).reshape( self.height, self.width )
+
+			# Prepare image for display
+			self.stereo_image[ 0:self.height, 0:self.width ] = image_1
+			self.stereo_image[ 0:self.height, self.width:2*self.width ] = image_2
+			
+			# Resize image for display
+			image_final = cv2.resize( self.stereo_image, None, fx=0.3, fy=0.3 )
+
+			# Display the image (scaled down)
+			cv2.imshow( "Stereo Cameras", image_final )
+			
+			# Keyboard interruption
+			key = cv2.waitKey(1) & 0xFF
+			
+			# Escape key
+			if key == 27 :
+				
+				# Exit live display
+				break
+				
+			# Space key
+			elif key == 32 :
+				
+				# Save images to disk 
+				image_count += 1
+				print( 'Save images {} to disk...'.format(image_count) )
+				cv2.imwrite( 'camera1-{:0>2}.png'.format(image_count), image_1 )
+				cv2.imwrite( 'camera2-{:0>2}.png'.format(image_count), image_2 )
+
+
+		# Stop image acquisition
+		self.camera_1.CaptureStop()
+		self.camera_2.CaptureStop()
+					
+		# Cleanup OpenCV
+		cv2.destroyAllWindows()
+
+
