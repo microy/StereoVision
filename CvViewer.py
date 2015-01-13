9b0ad6a897e14b9be2bced60bdebb6205800b06b
Michaël Roy <microygh@gmail.com>
2015-01-14 00:09:29 +0100

    Remove frame queue, and rename capture functions.

--- CvViewer.py
+++ CvViewer.py
@@ -41,24 +41,21 @@ class Viewer( object ) :
 
 		# Start image acquisition
 		self.capturing = True
-		self.camera.CaptureStart( self.ImageCallback )
+		self.camera.StartCapture( self.ImageCallback )
 		
-		# Keyboard interruption
+		# Streaming loop
 		while self.capturing : pass
 
 		# Stop image acquisition
-		self.camera.CaptureStop()
+		self.camera.StopCapture()
 
 		# Cleanup OpenCV
-		cv2.destroyWindow( self.camera.id_string )
+		cv2.destroyAllWindows()
 
 	#
 	# Display the current image
 	#
-	def ImageCallback( self ) :
-		
-		# Fetch the camera image
-		image = self.camera.GetImage()
+	def ImageCallback( self, image ) :
 		
 		# Resize image for display
 		image_displayed = cv2.resize( image, None, fx=0.3, fy=0.3 )
@@ -93,15 +90,6 @@ class StereoViewer( object ) :
 		self.width = camera_1.width
 		self.height = camera_1.height
 		
-		# Lock for data synchronisation
-		self.lock = threading.Lock()
-
-		# Live image of both cameras
-		self.stereo_image = np.zeros( (self.height, 2*self.width), dtype=np.uint8 )
-		
-		# Number of images saved
-		self.image_count = 0
-		
 	#
 	# Start capture and display image stream
 	#
@@ -109,15 +97,15 @@ class StereoViewer( object ) :
 
 		# Start image acquisition
 		self.capturing = True
-		self.camera_1.CaptureStart( self.ImageCallback_1 )
-		self.camera_2.CaptureStart( self.ImageCallback_2 )
+		self.camera_1.StartCapture( self.ImageCallback_1 )
+		self.camera_2.StartCapture( self.ImageCallback_2 )
 		
-		# Keyboard interruption
+		# Streaming loop
 		while self.capturing : pass
 
 		# Stop image acquisition
-		self.camera_1.CaptureStop()
-		self.camera_2.CaptureStop()
+		self.camera_1.StopCapture()
+		self.camera_2.StopCapture()
 
 		# Cleanup OpenCV
 		cv2.destroyAllWindows()
@@ -125,25 +113,13 @@ class StereoViewer( object ) :
 	#
 	# Display the current image for camera 1
 	#
-	def ImageCallback_1( self ) :
-		
-		# Fetch the camera image
-		image = self.camera.GetImage()
+	def ImageCallback_1( self, image ) :
 
-		# Lock the thread
-		self.lock.acquire()
-		
-		# Put the current image in the combined image
-		self.stereo_image[ 0:self.height, 0:self.width ] = image
-		
 		# Resize image for display
-		image_displayed = cv2.resize( self.stereo_image, None, fx=0.3, fy=0.3 )
+		image_displayed = cv2.resize( image, None, fx=0.3, fy=0.3 )
 		
-		# Release the thread
-		self.lock.release()
-
 		# Display the image (scaled down)
-		cv2.imshow( "Stereo", image_displayed )
+		cv2.imshow( "Camera 1", image_displayed )
 
 		# Keyboard interruption
 		if ( cv2.waitKey(1) & 0xFF ) == 27 :
@@ -152,25 +128,13 @@ class StereoViewer( object ) :
 	#
 	# Display the current image for camera 2
 	#
-	def ImageCallback_2( self ) :
-		
-		# Fetch the camera image
-		image = self.camera.GetImage()
-
-		# Lock the thread
-		self.lock.acquire()
-		
-		# Put the current image in the combined image
-		self.stereo_image[ 0:self.height, self.width:2*self.width ] = image
+	def ImageCallback_2( self, image ) :
 
 		# Resize image for display
-		image_displayed = cv2.resize( self.stereo_image, None, fx=0.3, fy=0.3 )
-		
-		# Release the thread
-		self.lock.release()
+		image_displayed = cv2.resize( image, None, fx=0.3, fy=0.3 )
 
 		# Display the image (scaled down)
-		cv2.imshow( "Stereo", image_displayed )
+		cv2.imshow( "Camera 2", image_displayed )
 
 		# Keyboard interruption
 		if ( cv2.waitKey(1) & 0xFF ) == 27 :
