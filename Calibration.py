3f05ed1288c43193b9d4161cd2c3958140f20711
Michaël Roy <microygh@gmail.com>
2015-01-14 17:09:38 +0100

    Add resize option to PreviewChessboard.

--- Calibration.py
+++ Calibration.py
@@ -15,22 +15,27 @@ import cv2
 #
 # Find the chessboard quickly and draw it
 #
-def PreviewChessboard( image, pattern_size = ( 9, 6 ) ) :
+def PreviewChessboard( image, pattern_size = ( 9, 6 ), resize = False ) :
+	
+	# Resize the image
+	if resize : small_image = cv2.resize( image, None, fx=0.3, fy=0.3 )
+	else : small_image = image
 	
 	# Find the chessboard corners on the image
-	found_all, corners = cv2.findChessboardCorners( image, pattern_size, flags = cv2.CALIB_CB_FAST_CHECK )
-		
+	found_all, corners = cv2.findChessboardCorners( small_image, pattern_size, flags = cv2.CALIB_CB_FAST_CHECK )	
+
 	# Chessboard found
 	if found_all :
 		
 		# Convert grayscale image in color
-		image = cv2.cvtColor( image, cv2.COLOR_GRAY2BGR )
+		small_image = cv2.cvtColor( small_image, cv2.COLOR_GRAY2BGR )
 
 		# Draw the chessboard corners on the image
-		cv2.drawChessboardCorners( image, pattern_size, corners, found_all )
+		cv2.drawChessboardCorners( small_image, pattern_size, corners, found_all )
 		
-		cv2.imshow( "Calibration", image )
+		cv2.imshow( 'Calibration', small_image )
 		cv2.waitKey(1)
+
 
 
 #
