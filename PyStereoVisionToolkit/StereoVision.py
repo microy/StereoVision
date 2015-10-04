a1e8d73e8462a92a87fd8239dc6f0e8feaadf8e5
Michaël Roy <microygh@gmail.com>
2015-10-04 16:18:43 +0200

    Enhance the user interface with shortcuts and icons.

--- PyStereoVisionToolkit/StereoVision.py
+++ PyStereoVisionToolkit/StereoVision.py
@@ -51,27 +51,36 @@ class StereoVision( QtGui.QWidget ) :
 		# Camera viewer
 		self.camera = Camera.QtCameraViewer( self, self.calibration )
 
-		# Buttons
+		# Widget elements
 		self.button_cross = QtGui.QPushButton( 'Cross', self )
+		self.button_cross.setCheckable( True )
+		self.button_cross.setShortcut( 'F1' )
 		self.button_cross.clicked.connect( self.Cross )
 		self.button_chessboard = QtGui.QPushButton( 'Chessboard', self )
+		self.button_chessboard.setCheckable( True )
+		self.button_chessboard.setShortcut( 'F2' )
 		self.button_chessboard.clicked.connect( self.Chessboard )
-		self.button_save = QtGui.QPushButton( 'Save', self )
-		self.button_save.clicked.connect( self.Save )
 		self.button_calibration = QtGui.QPushButton( 'Calibration', self )
+		self.button_calibration.setShortcut( 'F3' )
+		if self.calibration : self.button_calibration.setIcon( self.style().standardIcon( QtGui.QStyle.SP_DialogYesButton ) )
+		else : self.button_calibration.setIcon( self.style().standardIcon( QtGui.QStyle.SP_DialogNoButton ) )
 		self.button_calibration.clicked.connect( self.Calibration )
 		self.button_disparity = QtGui.QPushButton( 'Disparity', self )
+		self.button_disparity.setCheckable( True )
+		self.button_disparity.setShortcut( 'F4' )
 		self.button_disparity.clicked.connect( self.Disparity )
 		self.button_reconstruction = QtGui.QPushButton( 'Reconstruction', self )
+		self.button_reconstruction.setShortcut( 'F5' )
 		self.button_reconstruction.clicked.connect( self.Reconstruction )
-		
-		# Calibration pattern size
 		self.spinbox_pattern_rows = QtGui.QSpinBox( self )
 		self.spinbox_pattern_rows.setValue( Calibration.pattern_size[0] )
 		self.spinbox_pattern_rows.valueChanged.connect( self.UpdatePatternSize )
 		self.spinbox_pattern_cols = QtGui.QSpinBox( self )
 		self.spinbox_pattern_cols.setValue( Calibration.pattern_size[1] )
 		self.spinbox_pattern_cols.valueChanged.connect( self.UpdatePatternSize )
+		self.button_save = QtGui.QPushButton( 'Save', self )
+		self.button_save.setShortcut( 'Space' )
+		self.button_save.clicked.connect( self.Save )
 
 		# Widget layout
 		self.layout_pattern_size = QtGui.QHBoxLayout()
@@ -81,17 +90,20 @@ class StereoVision( QtGui.QWidget ) :
 		self.layout_controls = QtGui.QHBoxLayout()
 		self.layout_controls.addWidget( self.button_cross )
 		self.layout_controls.addWidget( self.button_chessboard )
-		self.layout_controls.addWidget( self.button_save )
 		self.layout_controls.addWidget( self.button_calibration )
 		self.layout_controls.addWidget( self.button_disparity )
 		self.layout_controls.addWidget( self.button_reconstruction )
 		self.layout_controls.addLayout( self.layout_pattern_size )
+		self.layout_controls.addWidget( self.button_save )
 		self.layout_global = QtGui.QVBoxLayout( self )
 		self.layout_global.addWidget( self.camera )
 		self.layout_global.addLayout( self.layout_controls )
+		
+		# Set the Escape key to close the application
+		QtGui.QShortcut( QtGui.QKeySequence( QtCore.Qt.Key_Escape ), self ).activated.connect( self.close )
 
 	#
-	# Chessboard finder
+	# Activate the cross on the images
 	#
 	def Cross( self ) :
 
@@ -104,21 +116,6 @@ class StereoVision( QtGui.QWidget ) :
 
 		self.camera.chessboard_enabled = not self.camera.chessboard_enabled
 
-
-
-	def Save( self ) :
-		
-		# Save images to disk 
-		current_time = time.strftime( '%Y%m%d_%H%M%S' )
-		print( 'Save images {} to disk...'.format(current_time) )
-		if self.camera.chessboard_enabled :
-			cv2.imwrite( '{}/left-{}.png'.format(Calibration.calibration_directory, current_time), self.camera.image_left )
-			cv2.imwrite( '{}/right-{}.png'.format(Calibration.calibration_directory, current_time), self.camera.image_right )
-		else :
-			cv2.imwrite( 'left-{}.png'.format(current_time), self.camera.image_left )
-			cv2.imwrite( 'right-{}.png'.format(current_time), self.camera.image_right )
-
-
 	#
 	# Stereo camera calibration
 	#
@@ -127,9 +124,7 @@ class StereoVision( QtGui.QWidget ) :
 		# Calibrate the stereo cameras
 		self.calibration = Calibration.StereoCameraCalibration()
 		self.camera.calibration = self.calibration
-		msgBox = QtGui.QMessageBox()
-		msgBox.setText( 'Calibration done !' )
-		msgBox.exec_()
+		self.button_calibration.setIcon( self.style().standardIcon( QtGui.QStyle.SP_DialogYesButton ) )
 
 	#
 	# Disparity map
@@ -154,6 +149,21 @@ class StereoVision( QtGui.QWidget ) :
 		
 		# Get the calibration pattern dimensions
 		Calibration.pattern_size = ( self.spinbox_pattern_rows.value(), self.spinbox_pattern_cols.value() )
+
+	#
+	# Save the stereo images
+	#
+	def Save( self ) :
+		
+		# Save images to disk 
+		current_time = time.strftime( '%Y%m%d_%H%M%S' )
+		print( 'Save images {} to disk...'.format(current_time) )
+		if self.camera.chessboard_enabled :
+			cv2.imwrite( '{}/left-{}.png'.format(Calibration.calibration_directory, current_time), self.camera.image_left )
+			cv2.imwrite( '{}/right-{}.png'.format(Calibration.calibration_directory, current_time), self.camera.image_right )
+		else :
+			cv2.imwrite( 'left-{}.png'.format(current_time), self.camera.image_left )
+			cv2.imwrite( 'right-{}.png'.format(current_time), self.camera.image_right )
 
 
 #
