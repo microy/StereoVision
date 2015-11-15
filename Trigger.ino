6ae7f1dc67cb83a35ede9ff9cd7cc38cfb7efe98
Michaël Roy <microygh@gmail.com>
2015-11-15 20:06:05 +0100

    Add start-stop button.

--- Trigger.ino
+++ Trigger.ino
@@ -1,23 +1,46 @@
 
 //
-// Generate a 9Hz square signal to trigger camera and lightning
+// Generate a 9Hz signal to trigger camera and lightning
 //
+
+// Setup
+int button = 7;
+int trigger = 13;
+int high_duration = 11;
+int low_duration = 100;
+boolean running = false;
+int button_state;
+int last_button_state = LOW;
 
 // Arduino setup
 void setup()
 {
+  // Input signal
+  pinMode( button, INPUT );
   // Output signal
-  pinMode( 13, OUTPUT );
+  pinMode( trigger, OUTPUT );
 }
 
 // Main loop
 void loop()
 {
-  // High state (11ms)
-  digitalWrite( 13, HIGH );
-  delay( 11 );
+  // Start / Stop button
+  button_state = digitalRead( button );
+  if( button_state == HIGH && last_button_state == LOW )
+  {
+    running = !running;
+    delay( 50 );
+  }
+  last_button_state = button_state;
+  // Trigger
+  if( running )
+  {
+    // High state
+    digitalWrite( trigger, HIGH );
+    delay( high_duration );
 
-  // Low state (100ms)
-  digitalWrite( 13, LOW );
-  delay( 100 );
+    // Low state
+    digitalWrite( trigger, LOW );
+    delay( low_duration );
+  }
 }
