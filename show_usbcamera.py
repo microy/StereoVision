2a086200b7644c3b3b869359c23366e7a3f36141
Michaël Roy <microygh@gmail.com>
2015-11-18 13:11:33 +0100

    Add OpenCV viewer for debug.

--- show_usbcamera.py
+++ show_usbcamera.py
@@ -10,9 +10,19 @@
 #
 # External dependencies
 #
-import sys
-from PySide import QtGui
-import VisionToolkit as vtk
+#import sys
+import cv2
+#from PySide import QtGui
+import VisionToolkit as vt
+
+
+#
+# Image callback function
+#
+def Callback( image ) :
+	# Display the stereo image
+	cv2.imshow( 'Camera', image )
+	cv2.waitKey( 1 )
 
 
 #
@@ -20,7 +30,23 @@ import VisionToolkit as vtk
 #
 if __name__ == '__main__' :
 
-    application = QtGui.QApplication( sys.argv )
-    widget = vtk.UsbCameraWidget()
-    widget.show()
-    sys.exit( application.exec_() )
+#    application = QtGui.QApplication( sys.argv )
+#    widget = vt.UsbCameraWidget()
+#    widget.show()
+#    sys.exit( application.exec_() )
+
+	# Initialize the stereo cameras
+	usbcamera = vt.UsbCamera( Callback )
+	# Lower the camera frame rate and resolution
+	usbcamera.camera.set( cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640 )
+	usbcamera.camera.set( cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480 )
+	usbcamera.camera.set( cv2.cv.CV_CAP_PROP_FPS, 25 )
+	# Start capture
+	usbcamera.start()
+	# Wait for user key press
+	raw_input( 'Press <enter> to stop the capture...' )
+	# Stop image acquisition
+	usbcamera.running = False
+	usbcamera.join()
+	# Cleanup OpenCV
+	cv2.destroyAllWindows()
