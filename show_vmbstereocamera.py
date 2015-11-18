e582a8632409cdf5625b51978e742ca9282c3d6f
Michaël Roy <microygh@gmail.com>
2015-11-18 02:56:07 +0100

    Add OpenCV display for debug.

--- show_vmbstereocamera.py
+++ show_vmbstereocamera.py
@@ -11,8 +11,23 @@
 # External dependencies
 #
 import sys
-from PySide import QtGui
+import cv2
+import numpy as np
+#from PySide import QtGui
 import VisionToolkit as vt
+
+
+#
+# Image callback function
+#
+def Callback( frame_left, frame_right ) :
+	# Put images side by side
+	stereo_image = np.concatenate( ( frame_left.image, frame_right.image ), axis = 1 )
+	# Resize image for display
+	stereo_image = cv2.resize( stereo_image, None, fx=0.4, fy=0.4 )
+	# Display the stereo image
+	cv2.imshow( 'StereoVision', stereo_image )
+	cv2.waitKey( 1 )
 
 
 #
@@ -20,7 +35,26 @@ import VisionToolkit as vt
 #
 if __name__ == '__main__' :
 
-	application = QtGui.QApplication( sys.argv )
-	widget = vt.VmbStereoCameraWidget( '50-0503326223', '50-0503323406' )
-	widget.show()
-	sys.exit( application.exec_() )
+#	application = QtGui.QApplication( sys.argv )
+#	widget = vt.VmbStereoCameraWidget( '50-0503326223', '50-0503323406' )
+#	widget.show()
+#	sys.exit( application.exec_() )
+
+	# Initialize the Vimba driver
+	vt.VmbStartup()
+	# Initialize the stereo cameras
+	camera = vt.VmbStereoCamera( '50-0503326223', '50-0503323406' )
+	# Connect the cameras
+	camera.Open()
+	# Start image acquisition
+	camera.StartCapture( Callback )
+	# Wait for user key press
+	raw_input( 'Press enter to stop the capture...' )
+	# Stop image acquisition
+	camera.StopCapture()
+	# Disconnect the camera
+	camera.Close()
+	# Shutdown Vimba
+	vt.VmbShutdown()
+	# Cleanup OpenCV
+	cv2.destroyAllWindows()
