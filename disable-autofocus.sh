a2023528368f41c747961690ebabac4cf0cd58c2
Michaël Roy <microygh@gmail.com>
2016-04-05 16:54:49 +0200

    Add Power line frequency setttings.

--- disable-autofocus.sh
+++ disable-autofocus.sh
@@ -12,3 +12,6 @@ uvcdynctrl -v -d video1 --set='Focus, Auto' 0
 uvcdynctrl -v -d video0 --set='Focus (absolute)' 30
 uvcdynctrl -v -d video1 --set='Focus (absolute)' 30
 
+# Fix power line frequency
+uvcdynctrl -v -d video0 --set='Power Line Frequency' 1
+uvcdynctrl -v -d video1 --set='Power Line Frequency' 1
